c0e4552dfbcfe436469b15b0827c78e6dee59a94
Scott Joyner <47367649+scottjoyner@users.noreply.github.com>
2021-04-18 16:49:46 -0400

    Update scjoyner_hw5.py

--- scjoyner_hw5.py
+++ scjoyner_hw5.py
@@ -1,3 +1,12 @@
 from nltk.corpus import wordnet
-syns = wordnet.synsets("program")
-print(syns)
+# syns = wordnet.synsets("program")
+
+with open("./policies/PBS_Kids.txt", "r") as pbs:
+  for x in pbs:
+    print(x)
+with open("./policies/gamebra.txt", "r") as gamebra:
+  for x in gamebra:
+    print(x)
+with open("./policies/rvappstudios.txt", "r") as rv:
+  for x in rv:
+    print(x)
